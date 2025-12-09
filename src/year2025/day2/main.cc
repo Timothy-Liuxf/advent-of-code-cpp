2f433c1aea9c729ea85a6146a941b9d715f43107
Timothy Liu <liuxf19@163.com>
2025-12-10 02:43:55 +0800

    chore(year2025): :bulb: add comments for year2025 day2 part2

--- src/year2025/day2/main.cc
+++ src/year2025/day2/main.cc
@@ -19,6 +19,34 @@ using namespace ADVENT_OF_CODE_CPP_NAMESPACE;
 
 using ID = std::uint64_t;
 
+/*
+ * The solution to Part 2 can be seen on LeetCode:
+ *     https://leetcode.com/problems/repeated-substring-pattern/
+ * The offical Chinese (Simplified) website:
+ *     https://leetcode.cn/problems/repeated-substring-pattern/solutions/386481/zhong-fu-de-zi-zi-fu-chuan-by-leetcode-solution/
+ *   provided four different solutions.
+ * Here I adopted the simpliest solution (the 2nd solution).
+ * However, the fastest solution is the 4th solution, as below:
+ *     bool KMP(const string& pattern) {
+ *         int n = pattern.size();
+ *         vector<int> fail(n, -1);
+ *         for (int i = 1; i < n; ++i) {
+ *             int j = fail[i - 1];
+ *             while (j != -1 && pattern[j + 1] != pattern[i]) {
+ *                 j = fail[j];
+ *             }
+ *             if (pattern[j + 1] == pattern[i]) {
+ *                 fail[i] = j + 1;
+ *             }
+ *         }
+ *         return fail[n - 1] != -1 && n % (n - fail[n - 1] - 1) == 0;
+ *     }
+ *
+ *     bool IsInvalidID(ID id) {
+ *         return KMP(to_string(id));
+ *     }
+ */
+
 [[nodiscard]] static ID IsInvalidID(ID id, std::uint64_t part) {
     auto id_str = std::to_string(id);
     if (part == 2) {
