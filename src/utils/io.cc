ab0b4ff85367af620d493a971f7af9bdc9957796
Timothy Liu <liuxf19@163.com>
2025-12-09 23:39:57 +0800

    chore(deps): :heavy_minus_sign: remove dependency of libcurl

--- src/utils/io.cc
+++ src/utils/io.cc
@@ -3,12 +3,12 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <mutex>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+// #include <mutex>
 
-#include <curl/curl.h>
+// #include <curl/curl.h>
 
 #include <common/common.hpp>
 
@@ -16,60 +16,62 @@ ADVENT_OF_CODE_CPP_UTILS_NAMESPACE_BEGIN
 
 using namespace std::literals::string_literals;
 
-namespace {
-std::once_flag curl_initialized = {};
+// namespace {
+// std::once_flag curl_initialized = {};
 
-class Curl {
-public:
-    Curl() : curl_(::curl_easy_init()) {
-        if (this->curl_ == nullptr) [[unlikely]] {
-            throw std::runtime_error("Failed to initialize CURL");
-        }
-    }
+// class Curl {
+// public:
+//     Curl() : curl_(::curl_easy_init()) {
+//         if (this->curl_ == nullptr) [[unlikely]] {
+//             throw std::runtime_error("Failed to initialize CURL");
+//         }
+//     }
 
-    [[nodiscard]] std::string GetContentsFromUrl(const std::string& url) {
-        std::string read_buffer;
+//     [[nodiscard]] std::string GetContentsFromUrl(const std::string& url) {
+//         std::string read_buffer;
 
-        ::curl_easy_setopt(curl_, ::CURLOPT_URL, url.c_str());
-        ::curl_easy_setopt(curl_, ::CURLOPT_FOLLOWLOCATION, 1L);
-        ::curl_easy_setopt(
-            curl_, ::CURLOPT_WRITEFUNCTION,
-            +[](char* ptr, [[maybe_unused]] size_t size, size_t nmemb, void* data) -> size_t {
-                std::string& read_buffer = *reinterpret_cast<std::string*>(data);
-                read_buffer.append(ptr, nmemb);
-                return nmemb;
-            });
-        ::curl_easy_setopt(curl_, ::CURLOPT_WRITEDATA, &read_buffer);
+//         ::curl_easy_setopt(curl_, ::CURLOPT_URL, url.c_str());
+//         ::curl_easy_setopt(curl_, ::CURLOPT_FOLLOWLOCATION, 1L);
+//         ::curl_easy_setopt(
+//             curl_, ::CURLOPT_WRITEFUNCTION,
+//             +[](char* ptr, [[maybe_unused]] size_t size, size_t nmemb, void* data) -> size_t {
+//                 std::string& read_buffer = *reinterpret_cast<std::string*>(data);
+//                 read_buffer.append(ptr, nmemb);
+//                 return nmemb;
+//             });
+//         ::curl_easy_setopt(curl_, ::CURLOPT_WRITEDATA, &read_buffer);
 
-        ::CURLcode res = ::curl_easy_perform(curl_);
-        if (res != ::CURLE_OK) [[unlikely]] {
-            throw std::runtime_error("CURL request failed: "s + std::string(::curl_easy_strerror(res)));
-        }
+//         ::CURLcode res = ::curl_easy_perform(curl_);
+//         if (res != ::CURLE_OK) [[unlikely]] {
+//             throw std::runtime_error("CURL request failed: "s + std::string(::curl_easy_strerror(res)));
+//         }
 
-        return read_buffer;
-    }
+//         return read_buffer;
+//     }
 
-    ~Curl() {
-        if (curl_ != nullptr) [[unlikely]] {
-            ::curl_easy_cleanup(curl_);
-        }
-    }
+//     ~Curl() {
+//         if (curl_ != nullptr) [[unlikely]] {
+//             ::curl_easy_cleanup(curl_);
+//         }
+//     }
 
-    [[nodiscard]] ::CURL* get() const {
-        return curl_;
-    }
+//     [[nodiscard]] ::CURL* get() const {
+//         return curl_;
+//     }
 
-private:
-    ::CURL* curl_;
-};
-}  // namespace
+// private:
+//     ::CURL* curl_;
+// };
+// }  // namespace
 
 [[nodiscard]] std::string GetContentsFromUrl(const std::string& url) {
-    std::call_once(curl_initialized, []() {
-        ::curl_global_init(CURL_GLOBAL_DEFAULT);
-    });
+    // std::call_once(curl_initialized, []() {
+    //     ::curl_global_init(CURL_GLOBAL_DEFAULT);
+    // });
 
-    return Curl {}.GetContentsFromUrl(url);
+    // return Curl {}.GetContentsFromUrl(url);
+    (void)url;
+    throw std::runtime_error("Get input directly from URL has not been implemented yet."s);
 }
 
 [[nodiscard]] std::string GetContentsFromFile(const std::filesystem::path& path) {
