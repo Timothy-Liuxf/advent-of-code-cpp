86fda528d6705d33487c53478e7219426dddf9b9
Timothy Liu <liuxf19@163.com>
2025-12-10 03:41:19 +0800

    feat(year2025): :sparkles: finish year2025 day4 part2

--- src/year2025/day4/main.cc
+++ src/year2025/day4/main.cc
@@ -17,17 +17,7 @@ using namespace std::literals::string_literals;
 using namespace std::literals::string_view_literals;
 using namespace ADVENT_OF_CODE_CPP_NAMESPACE;
 
-static std::string Solve(std::uint64_t part, std::string_view input) {
-    const auto diagram = std::views::split(input, "\n"sv) | std::views::filter([](auto &&line) {
-                             return std::ranges::distance(line) != 0;
-                         }) |
-                         std::views::transform([](auto &&line) {
-                             return line | std::views::transform([](auto ch) {
-                                        return ch == '@' ? 1 : 0;
-                                    }) |
-                                    std::ranges::to<std::vector<int>>();
-                         }) |
-                         std::ranges::to<std::vector<std::vector<int>>>();
+static std::uint64_t SolveImpl(std::vector<std::vector<int>> &diagram, int part) {
     const auto nrow     = (int)diagram.size();
     const auto ncol     = (int)diagram[0].size();
     const auto in_range = [nrow, ncol](int row, int col) {
@@ -43,31 +33,47 @@ static std::string Solve(std::uint64_t part, std::string_view input) {
         {1, 0},
         {1, 1},
     }};
-    if (part == 1) {
-        // Part 1
-        std::uint64_t result = 0;
-        for (int i = 0; i < nrow; ++i) {
-            for (int j = 0; j < ncol; ++j) {
-                if (diagram[i][j] == 0) {
-                    continue;
+
+    std::uint64_t result = 0;
+    for (int i = 0; i < nrow; ++i) {
+        for (int j = 0; j < ncol; ++j) {
+            if (diagram[i][j] == 0) {
+                continue;
+            }
+            std::uint64_t cnt = 0;
+            for (const auto &direct : directions) {
+                const auto x = i + direct.first, y = j + direct.second;
+                if (in_range(x, y)) [[likely]] {
+                    cnt += diagram[x][y];
                 }
-                std::uint64_t cnt = 0;
-                for (const auto &direct : directions) {
-                    const auto x = i + direct.first, y = j + direct.second;
-                    if (in_range(x, y)) [[likely]] {
-                        cnt += diagram[x][y];
-                    }
-                }
-                if (cnt < 4) {
-                    ++result;
+            }
+            if (cnt < 4) {
+                ++result;
+                if (part != 1) {
+                    diagram[i][j] = 0;
                 }
             }
         }
-        return std::to_string(result);
-    } else {
-        // Part 2
-        throw std::runtime_error("Part 2 not implemented.");
     }
+    if (part == 1 || result == 0) {
+        return result;
+    } else {
+        return result + SolveImpl(diagram, part);
+    }
+}
+
+static std::string Solve(std::uint64_t part, std::string_view input) {
+    auto diagram = std::views::split(input, "\n"sv) | std::views::filter([](auto &&line) {
+                       return std::ranges::distance(line) != 0;
+                   }) |
+                   std::views::transform([](auto &&line) {
+                       return line | std::views::transform([](auto ch) {
+                                  return ch == '@' ? 1 : 0;
+                              }) |
+                              std::ranges::to<std::vector<int>>();
+                   }) |
+                   std::ranges::to<std::vector<std::vector<int>>>();
+    return std::to_string(SolveImpl(diagram, part));
 }
 
 ADVENT_OF_CODE_CPP_MAIN_FUNCTION(Solve)
