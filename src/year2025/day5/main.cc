29cb535820f3231a352b69cbf172b980e4ff3807
Timothy Liu <liuxf19@163.com>
2025-12-10 11:24:58 +0800

    refactor(year2025): :recycle: extract merging ranges in day5

--- src/year2025/day5/main.cc
+++ src/year2025/day5/main.cc
@@ -18,39 +18,45 @@ using namespace ADVENT_OF_CODE_CPP_NAMESPACE;
 using ID      = std::uint64_t;
 using IDRange = std::pair<ID, ID>;
 
+[[nodiscard]] static ADVENT_OF_CODE_CPP_ALWAYS_INLINE std::vector<IDRange> SortAndMergeRanges(
+    std::vector<IDRange>& ranges) {
+    std::ranges::sort(ranges, [](const IDRange& range1, const IDRange& range2) {
+        return range1.first < range2.first;
+    });
+    std::vector<IDRange> merged_ranges;
+    merged_ranges.reserve(ranges.size());
+    merged_ranges.emplace_back(ranges[0]);
+    for (auto i = 1uz; i < ranges.size(); ++i) {
+        if (ranges[i].first > merged_ranges.back().second) {
+            merged_ranges.emplace_back(ranges[i]);
+        } else {
+            merged_ranges.back().second = std::max(merged_ranges.back().second, ranges[i].second);
+        }
+    }
+    return merged_ranges;
+}
+
 static std::string Solve(std::uint64_t part, std::string_view input) {
     (void)input;
     if (part == 1) {
         // Part 1
         std::vector<IDRange> ranges;
         std::vector<IDRange> merged_ranges;
-        std::ranges::for_each(std::views::split(input, ","sv), [](auto&&) {});
-        auto result = std::ranges::count_if(
+        auto                 result = std::ranges::count_if(
             std::views::split(input, "\n"sv) | std::views::drop_while([&ranges, &merged_ranges](auto&& line) {
-                std::string_view line_sv  = std::string_view(&*line.begin(), std::ranges::distance(line));
+                std::string_view line_sv = std::string_view(&*line.begin(), std::ranges::distance(line));
                 const bool       is_empty = line_sv.empty();
                 if (!is_empty) [[likely]] {
                     if (const auto dash_pos = line_sv.find('-'); dash_pos == std::string_view::npos) [[unlikely]] {
                         throw std::runtime_error(std::format("Invalid id range: {}", line_sv));
                     } else {
                         const auto start_id = utils::StringViewToIntegral<ID>(line_sv.substr(0, dash_pos));
-                        const auto end_id   = utils::StringViewToIntegral<ID>(line_sv.substr(dash_pos + 1));
+                        const auto end_id = utils::StringViewToIntegral<ID>(line_sv.substr(dash_pos + 1));
                         ranges.emplace_back(start_id, end_id);
                     }
                     return true;
                 } else {
-                    std::ranges::sort(ranges, [](const IDRange& range1, const IDRange& range2) {
-                        return range1.first < range2.first;
-                    });
-                    merged_ranges.reserve(ranges.size());
-                    merged_ranges.emplace_back(ranges[0]);
-                    for (auto i = 1uz; i < ranges.size(); ++i) {
-                        if (ranges[i].first > merged_ranges.back().second) {
-                            merged_ranges.emplace_back(ranges[i]);
-                        } else {
-                            merged_ranges.back().second = std::max(merged_ranges.back().second, ranges[i].second);
-                        }
-                    }
+                    merged_ranges = SortAndMergeRanges(ranges);
                     return false;
                 }
             }),
@@ -83,19 +89,7 @@ static std::string Solve(std::uint64_t part, std::string_view input) {
                           return std::make_pair(start_id, end_id);
                       }) |
                       std::ranges::to<std::vector<IDRange>>();
-        std::ranges::sort(ranges, [](const IDRange& range1, const IDRange& range2) {
-            return range1.first < range2.first;
-        });
-        std::vector<IDRange> merged_ranges;
-        merged_ranges.reserve(ranges.size());
-        merged_ranges.emplace_back(ranges[0]);
-        for (auto i = 1uz; i < ranges.size(); ++i) {
-            if (ranges[i].first > merged_ranges.back().second) {
-                merged_ranges.emplace_back(ranges[i]);
-            } else {
-                merged_ranges.back().second = std::max(merged_ranges.back().second, ranges[i].second);
-            }
-        }
+        auto       merged_ranges = SortAndMergeRanges(ranges);
         const auto result = std::ranges::fold_left(merged_ranges, 0uz, [](std::uint64_t acc, const IDRange& range) {
             return acc + (range.second - range.first + 1);
         });
