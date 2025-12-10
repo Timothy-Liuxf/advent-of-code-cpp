f6f5536423110aec2a78e7aab7b329c51bb7c783
Timothy Liu <liuxf19@163.com>
2025-12-10 09:45:01 +0800

    refactor(cmd): :recycle: move exception handling from header to source file

--- src/utils/cmd.cc
+++ src/utils/cmd.cc
@@ -1,11 +1,12 @@
 #include "cmd.hpp"
 
 #include <cstdint>
+#include <cstdio>
+#include <exception>
 #include <format>
 #include <print>
 #include <stdexcept>
 #include <string>
-#include <string_view>
 
 #include <tclap/CmdLine.h>
 
@@ -14,37 +15,45 @@
 
 ADVENT_OF_CODE_CPP_UTILS_NAMESPACE_BEGIN
 
-void SolveProblemFromArgs(int argc, char* argv[], const std::string& title,
-                          std::function<std::string(std::uint64_t, std::string_view)> solver) {
-    TCLAP::CmdLine cmd(title, ' ', PACKAGE_VERSION);
+int SolveProblemFromArgs(int argc, char* argv[], const std::string& title, SolveFunctionType solver) noexcept {
+    try {
+        TCLAP::CmdLine cmd(title, ' ', PACKAGE_VERSION);
 
-    TCLAP::ValueArg<std::uint64_t> part_arg("p", "part", "Part number", false, 1, "1 or 2");
-    cmd.add(part_arg);
+        TCLAP::ValueArg<std::uint64_t> part_arg("p", "part", "Part number", false, 1, "1 or 2");
+        cmd.add(part_arg);
 
-    TCLAP::ValueArg<std::filesystem::path> file_arg("f", "file", "Path to input file", false, ".", "path");
-    cmd.add(file_arg);
+        TCLAP::ValueArg<std::filesystem::path> file_arg("f", "file", "Path to input file", false, ".", "path");
+        cmd.add(file_arg);
 
-    TCLAP::ValueArg<std::string> url_arg("u", "url", "URL to input file", false, ".", "url");
-    cmd.add(url_arg);
+        TCLAP::ValueArg<std::string> url_arg("u", "url", "URL to input file", false, ".", "url");
+        cmd.add(url_arg);
 
-    cmd.parse(argc, argv);
+        cmd.parse(argc, argv);
 
-    auto part = part_arg.getValue();
-    if (part != 1 && part != 2) {
-        throw std::invalid_argument(std::format("Part number can only be 1 or 2, got: {}", part));
+        auto part = part_arg.getValue();
+        if (part != 1 && part != 2) {
+            throw std::invalid_argument(std::format("Part number can only be 1 or 2, got: {}", part));
+        }
+
+        std::string result;
+        if (url_arg.isSet()) {
+            auto content = utils::GetContentsFromUrl(url_arg.getValue());
+            result       = solver(part, content);
+        } else if (file_arg.isSet()) {
+            auto content = utils::GetContentsFromFile(file_arg.getValue());
+            result       = solver(part, content);
+        } else {
+            throw std::invalid_argument("Either --file or --url must be specified.");
+        }
+        std::println(std::cout, "Answer of Part {}: {}", part_arg.getValue(), result);
+    } catch (std::exception& e) {
+        std::fprintf(stderr, "Error: %s\n", e.what());
+        return 1;
+    } catch (...) {
+        std::fprintf(stderr, "Unknown error occurred.\n");
+        return 1;
     }
-
-    std::string result;
-    if (url_arg.isSet()) {
-        auto content = utils::GetContentsFromUrl(url_arg.getValue());
-        result       = solver(part, content);
-    } else if (file_arg.isSet()) {
-        auto content = utils::GetContentsFromFile(file_arg.getValue());
-        result       = solver(part, content);
-    } else {
-        throw std::invalid_argument("Either --file or --url must be specified.");
-    }
-    std::println(std::cout, "Answer of Part {}: {}", part_arg.getValue(), result);
+    return 0;
 }
 
 ADVENT_OF_CODE_CPP_UTILS_NAMESPACE_END
--- src/utils/cmd.hpp
+++ src/utils/cmd.hpp
@@ -1,35 +1,25 @@
 #ifndef ADVENT_OF_CODE_CPP_UTILS_CMD_HPP
 #define ADVENT_OF_CODE_CPP_UTILS_CMD_HPP
 
-#include <config.h>  // Required by the Macro function ADVENT_OF_CODE_CPP_MAIN_FUNCTION
+#include <config.h>
 
-#include <cstdio>     // Required by the Macro function ADVENT_OF_CODE_CPP_MAIN_FUNCTION
-#include <exception>  // Required by the Macro funtion ADVENT_OF_CODE_CPP_MAIN_FUNCTION
-#include <functional>
+#include <cstdint>
+#include <string>
 #include <string_view>
-
-#include <tclap/CmdLine.h>
 
 #include "common.hpp"
 
 ADVENT_OF_CODE_CPP_UTILS_NAMESPACE_BEGIN
 
-void SolveProblemFromArgs(int argc, char* argv[], const std::string& title,
-                          std::function<std::string(std::uint64_t, std::string_view)> solver);
+using SolveFunctionType = std::string (*)(std::uint64_t, std::string_view);
+
+[[nodiscard]] int SolveProblemFromArgs(int argc, char* argv[], const std::string& title,
+                                       SolveFunctionType solver) noexcept;
 
 #define ADVENT_OF_CODE_CPP_MAIN_FUNCTION(year, day, solve_func)                                                        \
     int main(int argc, char* argv[]) {                                                                                 \
-        try {                                                                                                          \
-            ADVENT_OF_CODE_CPP_UTILS_NAMESPACE::SolveProblemFromArgs(                                                  \
-                argc, argv, PACKAGE_NAME ": year" #year " day" #day, solve_func);                                      \
-        } catch (std::exception & e) {                                                                                 \
-            std::fprintf(stderr, "Error: %s\n", e.what());                                                             \
-            return 1;                                                                                                  \
-        } catch (...) {                                                                                                \
-            std::fprintf(stderr, "Unknown error occurred.\n");                                                         \
-            return 1;                                                                                                  \
-        }                                                                                                              \
-        return 0;                                                                                                      \
+        return ADVENT_OF_CODE_CPP_UTILS_NAMESPACE::SolveProblemFromArgs(                                               \
+            argc, argv, PACKAGE_NAME ": year" #year " day" #day, solve_func);                                          \
     }
 
 ADVENT_OF_CODE_CPP_UTILS_NAMESPACE_END
