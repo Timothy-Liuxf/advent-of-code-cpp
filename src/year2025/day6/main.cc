844b0a01ba4bfb7e7d1680e0099a6cf5571e8b06
Timothy Liu <liuxf19@163.com>
2025-12-10 19:19:52 +0800

    feat(year2025): :sparkles: finish year2025 day6 part2

--- src/year2025/day6/main.cc
+++ src/year2025/day6/main.cc
@@ -17,7 +17,7 @@ using namespace std::literals::string_literals;
 using namespace std::literals::string_view_literals;
 using namespace ADVENT_OF_CODE_CPP_NAMESPACE;
 
-static std::string Solve(std::uint64_t part, const std::string& input) {
+[[nodiscard]] static std::string SolvePart1(const std::string& input) {
     std::istringstream sin(input);
     std::string        line;
     std::int64_t       result = 0;
@@ -55,12 +55,62 @@ static std::string Solve(std::uint64_t part, const std::string& input) {
             }
         }
     }
+    return std::to_string(result);
+}
+
+[[nodiscard]] static std::string SolvePart2(const std::string& input) {
+    auto numbers = input | std::views::split('\n') | std::views::filter([](auto&& line) {
+                       return !line.empty();
+                   }) |
+                   std::views::transform([](auto&& line) {
+                       return std::string(std::string_view(line));
+                   }) |
+                   std::ranges::to<std::vector<std::string>>();
+    const auto operators = numbers.back() | std::views::filter([](const char ch) {
+                               return ch != ' ';
+                           }) |
+                           std::ranges::to<std::vector<char>>();
+    numbers.pop_back();
+    const auto   ncol   = numbers[0].size();
+    std::int64_t result = 0, current_value = 0;
+    auto         op_pos = operators.size() - 1;
+
+    for (std::int64_t i = (std::int64_t)ncol - 1; i >= 0;) {
+        bool is_reading_number = false;
+        current_value          = operators[op_pos] == '+' ? 0 : 1;
+        for (; i >= 0; --i) {
+            std::int64_t number   = 0;
+            bool         is_blank = true;
+            for (const auto& row : numbers) {
+                if (row[i] != ' ') {
+                    number   = number * 10 + row[i] - '0';
+                    is_blank = false;
+                }
+            }
+            if (is_blank) {
+                if (!is_reading_number) {
+                    continue;
+                } else {
+                    result += current_value;
+                    current_value = 0;
+                    --i;
+                    --op_pos;
+                    break;
+                }
+            } else {
+                is_reading_number = true;
+                current_value     = operators[op_pos] == '+' ? current_value + number : current_value * number;
+            }
+        }
+    }
+    return std::to_string(result + current_value);
+}
+
+static std::string Solve(std::uint64_t part, const std::string& input) {
     if (part == 1) {
-        // Part 1
-        return std::to_string(result);
+        return SolvePart1(input);
     } else {
-        // Part 2
-        throw std::runtime_error("Not implemented yet.");
+        return SolvePart2(input);
     }
 }
 
